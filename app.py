0da2b1fb524a60ea0c284d80367413547d28a838
marccunill <marccunillpunti@gmail.com>
2025-04-24 21:37:27 +0200

    Add BigQuery integration to Streamlit app and update requirements

--- app.py
+++ app.py
@@ -1,4 +1,25 @@
 import streamlit as st
+from google.cloud import bigquery
+from google.oauth2 import service_account
 
-x = st.slider("Select a value")
-st.write(x, "squared is", x * x)
+
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"]
+)
+client = bigquery.Client(credentials=credentials)
+
+
+st.title("Consulta BigQuery desde Streamlit")
+
+query = """
+    SELECT *
+    FROM `autotiming-dev.metrics.fuel_type`
+"""
+
+# Ejecutar consulta
+query_job = client.query(query)
+df = query_job.to_dataframe()
+
+# Mostrar resultado
+st.write("Datos desde BigQuery:")
+st.dataframe(df)
